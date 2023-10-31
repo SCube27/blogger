2f3f560b9ecd982a8ea7bcf23df15d88b005bd4e
unknown <sahilshah2104@gmail.com>
2023-11-01 00:27:43 +0530

    Added dummy data and template extensions

--- blog.py
+++ blog.py
@@ -1,15 +1,32 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+
+# This is dummy data, it'll actually come from a database
+posts = [
+    {
+        'author':'Sahil Shah',
+        'title':'Real Love',
+        'content':'real love happens only once like Sahil has with Sakshi',
+        'date_posted':'31st October 2023'
+    },
+    {
+        'author':'Sakshi Paygude',
+        'title':'My Partner',
+        'content':'I am really fortunate to have found a real love and life partner in Sahil! I Love Him Alottt!',
+        'date_posted':'14th February 2024'
+    }
+]
 
 # Every route describes a page of the website
 @app.route("/") # The / in the brackets indicates home page
 # The function gets called when the page opens and whatever is in it runs 
 def love() :
-    return "<h1>Sakshi Loves Sahil <<<3</h1>" # Usually in websites the html files are returned over here
+    return render_template('home.html', posts=posts)
 
 @app.route("/about") # The /about is for the about page of the website
 def about() :
-    return "<h1>The story of Sakhil starts far early and is never ending and they both are inseparable forever! <3 As they always come to each other how apart they go</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
